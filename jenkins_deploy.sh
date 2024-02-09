8a1da6772cb1408891bd121ae720be5ac371c9d0
sreepathysois <54054574+sreepathysois@users.noreply.github.com>
2024-02-09 16:40:25 +0530

    Update jenkins_deploy.sh

--- jenkins_deploy.sh
+++ jenkins_deploy.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-sudo mkdir /var/www/html/bookalbum
-sudo cp -rf * /var/www/html/bookalbum/.
+#sudo mkdir /var/www/html/bookalbum
+#sudo cp -rf * /var/www/html/bookalbum/.
+# Destination directory
+DEST_DIR="/var/www/html/bookalbum"
+
+# Check if the destination directory exists
+if [ ! -d "$DEST_DIR" ]; then
+    # If the directory doesn't exist, create it
+    sudo mkdir -p "$DEST_DIR"
+    
+    # Copy files only if the destination directory was created
+    if [ $? -eq 0 ]; then
+        sudo cp -rf * "$DEST_DIR/"
+        echo "Files copied to $DEST_DIR."
+    else
+        echo "Failed to create directory $DEST_DIR."
+    fi
+else
+    echo "Directory $DEST_DIR already exists. Skipping copy."
+fi
 #echo "DocumentRoot /var/www/html/bookalbum" | sudo tee -a  /etc/apache2/sites-available/000-default.conf
 #sudo sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/html/bookalbum|' /etc/apache2/sites-available/000-default.conf
 sudo sed -E -i 's|DocumentRoot[[:space:]]+/var/www/html/[^[:space:]]*|DocumentRoot /var/www/html/bookalbum|' /etc/apache2/sites-available/000-default.conf
